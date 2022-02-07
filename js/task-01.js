f87a35d0a1b6ea354a010aff790541e7d602b850
Daryna <dashaobit@gmail.com>
2022-02-08 00:12:52 +0200

    Add task-02

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,13 @@
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item
-
+// ищем внутри document все элементы с классом .item. Получаем ссылку на .item
 const itemsRef = document.querySelectorAll('.item');
 console.log("Number of categories: ", itemsRef.length); // Number of categories: 3
 
 
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// ищем внутри class="item" .item h2  и item>ul>li
+// .item - родительский селектор, .item h2  и item>ul>li - потомки
+
 itemsRef.forEach(item => {
     const itemsTitle = item.querySelector('.item h2');
     console.log('Category: ', itemsTitle.textContent);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// ищем внутри document элемент с id-шником ingredients <ul id="ingredients">
+const ingredientListEl = document.querySelector("#ingredients");
+
+// сконструировали DOM-элемент
+// создаём новый массив arrIngredient с результатом вызова функции-колбек для каждого элемента массива ingredients
+
+const arrIngredient=ingredients.map((ingredient) =>{
+  const listEl=document.createElement('li');
+  listEl.classList.add('item');
+  listEl.textContent=ingredient;
+  console.log(listEl)
+
+  return listEl;
+});
+
+// добавили DOM-элемент на страницу
+ingredientListEl.append(...arrIngredient);
